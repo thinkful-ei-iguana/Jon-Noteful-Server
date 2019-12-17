d059c319e56971126a8a9c4409af6f3932ae264a
Jon <jonathanjackson424@gmail.com>
2019-12-17 15:23:23 -0500

    moved into server-side repo

---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,7 @@
+INSERT INTO noteful_notes (name, content, modified, folderId)
+VALUES
+  ('Dwight', 'assissant to the regional manager', now(), 1),
+  ('Jim', 'three hole punched jim', now(), 2),
+  ('Pam', 'artists', now(), 1),
+  ('golden face', 'Threat level midnight', now(), 2),
+  ('micheal', 'Hero', now(), 1);
